7ac5e581cbfaa613fa632eb19f5474b5937ef178
Noham Coulon <noham.coulon@gmail.com>
2025-04-01 14:11:01 +0200

    mérope follows player + flee when too close

---
+++ Assets/Script/MéropeFollow.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MéropeFollow : MonoBehaviour
+{
+    private PlayerScript _player;
+    
+    [SerializeField] private float speed = 1.0f;
+    [SerializeField] private float maxDistance = 5.5f;
+    [SerializeField] private float minDistance = 2.5f;
+    private Rigidbody _rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GameManager.Instance.GetPlayer();//GameObject.FindGameObjectWithTag("Player"); 
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // follow the player
+        if (_player != null)
+        {
+            // If too far from the player, move towards the player
+            Vector3 direction = _player.transform.position - transform.position;
+            direction.y = 0; // Keep the y-axis constant to avoid vertical movement
+            
+            float dist = (_player.transform.position - transform.position).magnitude;
+            if(dist > maxDistance)
+            {
+                transform.position += direction.normalized * speed * Time.deltaTime;
+            } else if (dist < minDistance)
+            {
+                // If too close to the player, move away from the player
+                transform.position -= direction.normalized * speed * Time.deltaTime;
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Player not found");
+        }
+    }
+}
