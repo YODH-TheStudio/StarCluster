750f2f9be8814c3a4c2bbdfdf6f3a92c8f9661d7
Noham Coulon <noham.coulon@gmail.com>
2025-04-02 10:30:53 +0200

    [UPD] rework

--- Assets/Script/Mérope/MéropeFollow.cs
+++ Assets/Script/Mérope/MéropeFollow.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -9,48 +12,51 @@ public class MéropeFollow : MonoBehaviour
     [SerializeField] private float maxDistance = 5.5f;
     [SerializeField] private float minDistance = 2.5f;
     [SerializeField] private float floatHeight = 1.5f;
-    [SerializeField] private float floatSpeed = 0.1f;
+    [SerializeField] private float floatCatchupSpeed = 0.3f;
+    [SerializeField] Vector3 maxOffset;
     
     private NavMeshAgent _agent;
     private Rigidbody _rb;
+
+    private Vector3 velocity;
+    
+    private List<Vector3> _path;
     
     // Start is called before the first frame update
     void Start()
     {
         _player = GameManager.Instance.GetPlayer();
         _agent = GetComponent<NavMeshAgent>();
+        _path = new List<Vector3>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        // Update the offset
+        float variance = (float)Math.Sin(Time.time);
+        Vector3 currentOffset = new Vector3(variance * maxOffset.x, variance * maxOffset.y, variance * maxOffset.z);
+        
         // follow the player
         if (_player != null)
         {
-            // If too far from the player, move towards the player
-            Vector3 direction = _player.transform.position - transform.position;
-            direction.y = 0; // Keep the y-axis constant to avoid vertical movement
-            
-            float dist = (_player.transform.position - transform.position).magnitude;
-            if(dist > maxDistance)
-            {
-                // If too far from the player, move towards them
-                _agent.SetDestination(_player.transform.position);
-                //transform.position += direction.normalized * speed * dist/5 * Time.deltaTime;
-            } else if (dist < minDistance)
-            {
-                // If too close to the player, move away
-                transform.position -= direction.normalized * speed * Time.deltaTime;
-            }
+            transform.position = Vector3.SmoothDamp(transform.position, _player.transform.position /*+ currentOffset*/, ref velocity, floatCatchupSpeed);
+ 
+            // if ((transform.position - _player.transform.position).magnitude < minDistance)
+            // {
+            //     // If too close to the player, move away
+            //     transform.position -= (transform.position - _player.transform.position).normalized * speed * Time.deltaTime;
+            // }
         }
         else
         {
             Debug.LogWarning("Player not found");
         }
-        
-        // Move up and down
-        Vector3 newPosition = transform.position;
-        newPosition.y = Mathf.Sin(Time.time * speed) * floatSpeed + floatHeight;
-        transform.position = newPosition;
+
+        // // Move up and down
+        // Vector3 newPosition = transform.position;
+        // newPosition.y = Mathf.Sin(Time.time * speed) * floatSpeed + floatHeight;
+        // transform.position = newPosition;
     }
 }
