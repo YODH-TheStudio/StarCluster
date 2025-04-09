0fdfba8df76cf3ccbe44200cb5299e51786c024b
Noham Coulon <noham.coulon@gmail.com>
2025-04-09 15:39:41 +0200

    [ADD] Custom grass shader for Terrain
    Will try to delete all unmodified files in the next commit

---
+++ Assets/Project/Script/Player/SendPositionToShader.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class SendPositionToShader : MonoBehaviour
+{
+    [SerializeField] private string shaderPropertyName = "_PlayerPosition";
+    void Update()
+    {
+        Vector3 playerPos = new Vector4(transform.position.x, transform.position.y, transform.position.z);
+        Shader.SetGlobalVector("_PlayerPosition", playerPos);
+        //Shader.SetGlobalVector(shaderPropertyName, transform.position);
+    }
+}
