59a30367828b49f5b43028dafdfbb9ec41b90305
William-Bailleul <wbailleul@gaming.tech>
2025-04-15 18:02:47 +0200

    [REF] Update Player Prefs

--- Assets/_Project/_Script/UI Menu/SettingsMenu.cs
+++ Assets/_Project/_Script/UI Menu/SettingsMenu.cs
@@ -1,10 +1,6 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
-using Systems.SceneManagement;
 
 public class SettingsMenu : Menu
 {
@@ -13,7 +9,7 @@ public class SettingsMenu : Menu
 
     private readonly List<string> _languages= new List<string> { "English", "French" };
     private string _currentLanguage;
-    private int _currentLanguageIndex = 0;
+    private int _currentLanguageIndex;
     
     [Range(0,1)]private float _masterVolume;
     [Range(0,1)]private float _musicVolume;
@@ -33,6 +29,7 @@ public class SettingsMenu : Menu
     private void LoadPlayerPrefs()
     {
         _currentLanguage = PlayerPrefs.GetString("Language", "en");
+        _currentLanguageIndex = PlayerPrefs.GetInt("Language", 0);
         _masterVolume = PlayerPrefs.GetFloat("MasterVolume", 0.5f);
         _musicVolume = PlayerPrefs.GetFloat("MusicVolume", 0.5f);
         _sfxVolume = PlayerPrefs.GetFloat("SFXVolume", 0.5f);
@@ -45,6 +42,7 @@ public class SettingsMenu : Menu
         _currentLanguageIndex = (_currentLanguageIndex++) % _languages.Count();
         _currentLanguage = _languages[_currentLanguageIndex];
         PlayerPrefs.SetString("Language", _currentLanguage);
+        PlayerPrefs.SetInt("LanguageIndex", _currentLanguageIndex);
     }
     
     public void MasterVolume(float volume)
