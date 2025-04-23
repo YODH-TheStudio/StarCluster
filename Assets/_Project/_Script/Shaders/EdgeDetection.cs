91506ea65c49c0701036105c6f5c79c1318cf0e2
Noham Coulon <noham.coulon@gmail.com>
2025-04-23 11:35:32 +0200

    Done

---
+++ Assets/_Project/_Script/Shaders/EdgeDetection.cs
@@ -0,0 +1,19 @@
+// using UnityEngine;
+// using UnityEngine.Rendering;
+// using UnityEngine.Rendering.Universal;
+//
+// public class EdgeDetection : ScriptableRendererFeature
+// {
+//     private class EdgeDetectionPass : ScriptableRenderPass
+//     {
+//         private Material material;
+//         
+//         private static readonly int OutlineThicknessProperty = Shader.PropertyToID("_OutlineThickness");
+//         private static readonly int OutlineColorProperty = Shader.PropertyToID("_OutlineColor");
+//
+//         public EdgeDetectionPass()
+//         {
+//             profilingSampler = new ProfilingSampler(nameof(EdgeDetectionPass));
+//         }
+//     }
+// }
