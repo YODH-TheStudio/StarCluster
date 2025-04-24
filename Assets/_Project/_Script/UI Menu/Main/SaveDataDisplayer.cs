7d1961db330f1ca8207c4dca109fdf8cfb8c7bd9
Noham Coulon <noham.coulon@gmail.com>
2025-04-24 13:46:16 +0200

    [ADD] Save should be working

--- Assets/_Project/_Script/UI Menu/Main/SaveDataDisplayer.cs
+++ Assets/_Project/_Script/UI Menu/Main/SaveDataDisplayer.cs
@@ -12,14 +12,14 @@ public class SaveDataDisplayer : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _saveDateTxt;
     [SerializeField] private RawImage _planetIcon;
     
-    private LocalizeStringEvent localizeStringEvent;
+    private LocalizeStringEvent _localizeStringEvent;
 
     private void Awake()
     {
         // Setup planet name localisation
         //localizeStringEvent = GetComponent<LocalizeStringEvent>();
-        localizeStringEvent = _planetNameTxt.GetComponent<LocalizeStringEvent>();
-        if (localizeStringEvent == null)
+        _localizeStringEvent = _planetNameTxt.GetComponent<LocalizeStringEvent>();
+        if (_localizeStringEvent == null)
         {
             Debug.LogError("LocalizeStringEvent component is missing on " + gameObject.name);
         }
@@ -33,9 +33,9 @@ public class SaveDataDisplayer : MonoBehaviour
     {
         //_planetNameTxt.text = planetNameKey;
         //Debug.LogAssertion("Setting planet name to " + planetNameKey);
-        localizeStringEvent.StringReference.TableReference = "Planet Names";
-        localizeStringEvent.StringReference.TableEntryReference = planetNameKey;
-        localizeStringEvent.RefreshString(); // Triggers the UI update
+        _localizeStringEvent.StringReference.TableReference = "Planet Names";
+        _localizeStringEvent.StringReference.TableEntryReference = planetNameKey;
+        _localizeStringEvent.RefreshString(); // Triggers the UI update
         
         _saveDateTxt.text = saveDate;
         if(planetIcon == null)
