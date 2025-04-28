0d91cdae74bbddd35993e5a6aab8499dc76e5785
Noham Coulon <44196664+coucoul38@users.noreply.github.com>
2025-04-28 18:26:38 +0200

    Update SavePosition.cs

--- Assets/_Project/_Script/Save and Load/SavePosition.cs
+++ Assets/_Project/_Script/Save and Load/SavePosition.cs
@@ -12,14 +12,13 @@ public class SavePosition : MonoBehaviour
         if (string.IsNullOrEmpty(identifier))
         {
             // generate a unique identifier
-            
             identifier = Guid.NewGuid().ToString();
         }
         // check if a gameObject has the same name
         while (NameInUse(identifier))
         {
             Debug.LogError("A gameObject with identifier " + identifier + " already exists. Generating a new one.");
-            // destroy this gameObject
+            // generate a new unique identifier
             identifier = Guid.NewGuid().ToString();
         }
         gameObject.name = identifier;
