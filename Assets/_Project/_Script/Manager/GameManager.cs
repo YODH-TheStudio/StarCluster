734548eb20e347217f9f80c17ddb3322737229fa
Urukagall <dany.jorge@free.fr>
2025-04-29 13:34:10 +0200

    [ADD] targetframerate

--- Assets/_Project/_Script/Manager/GameManager.cs
+++ Assets/_Project/_Script/Manager/GameManager.cs
@@ -48,6 +48,9 @@ public class GameManager : PersistentSingleton<GameManager>
     
     private void Start()
     {
+        QualitySettings.vSyncCount = 0; // Set vSyncCount to 0 so that using .targetFrameRate is enabled.
+        Application.targetFrameRate = 60;
+        
         EnhancedTouchSupport.Enable();
         ETouch.Touch.onFingerDown += Touch_OnFingerDown;
         ETouch.Touch.onFingerUp += Touch_OnFingerUp;
